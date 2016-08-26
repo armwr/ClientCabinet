49dad9df939a7f2bb0536c6b640a6ba491b5401f
Volodymyr Lysenko <eaglelegendary@mail.ru>
2016-08-26 13:35:33 +0300

    Edit client

--- scripts/main.js
+++ scripts/main.js
@@ -8,27 +8,9 @@
 	function myCtrl($http, clientsFactory, $scope) {
 
 		clientsFactory.getClients().then(function(clients) {
-			vm.data = clients.data
+			vm.data = clients.data 
 		})
 
 		var vm = this;
-		vm.editList = editList;
-		vm.saveEdit = saveEdit;
-
-		vm.editing;
-		vm.person;
-
-		function editList(person) {
-			vm.editing = true;
-			$scope.person = person;
-			console.log('Editing mode')
-		}
-
-		function saveEdit() {
-			vm.editing = false;
-			vm.person = {};
-			console.log('Edited!')
-		}
-
 	}
 })()
